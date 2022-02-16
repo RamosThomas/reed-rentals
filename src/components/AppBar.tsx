ccee3c11f7a6f4390f93978d7626c1664d5cd6e7
thom8047 <thom8047@bears.unco.edu>
2022-02-16 15:24:06 -0700

    Add AppBar Buttons for navigation purposes

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -2,21 +2,103 @@ import * as React from "react";
 
 import logo from "../img/logo.png";
 
-import { AppBar as MUIAppBar, Box, Toolbar } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import {
+  AppBar as MUIAppBar,
+  Box,
+  Button,
+  InputBase,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { styled, alpha } from "@mui/system";
 
-export default function AppBar() {
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha("#fff", 0.15),
+  "&:hover": {
+    backgroundColor: alpha("#fff", 0.25),
+  },
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(1),
+    width: "auto",
+  },
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: "0.25s",
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: "12ch",
+      "&:focus": {
+        width: "50ch",
+      },
+    },
+  },
+}));
+
+const StyledButton = styled(Button)(({ theme }) => ({
+  padding: theme.spacing(1, 2, 1, 2),
+  color: "#fff",
+}));
+
+interface Props {
+  onPageChange: React.Dispatch<React.SetStateAction<number>>;
+}
+
+export default function AppBar(props: Props) {
+  const { onPageChange } = props;
+
+  const handleClick = React.useCallback(
+    (page: number) => {
+      onPageChange(page);
+    },
+    [onPageChange]
+  );
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <MUIAppBar sx={{ backgroundColor: "#242526" }} position="static">
+      <MUIAppBar sx={{ backgroundColor: "#242526" }} position="sticky">
         <Toolbar>
-          <Box sx={{ width: "300px", mr: 2 }}>
+          <Box
+            onClick={() => handleClick(0)}
+            sx={{ cursor: "pointer", width: "300px", mr: 2 }}
+          >
             <img
               style={{ maxWidth: "100%", maxHeight: "100%" }}
               src={logo}
               alt="logo"
             />
           </Box>
-          SEARCH BAR
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase placeholder="Search…" />
+          </Search>
+
+          <StyledButton onClick={() => handleClick(1)}>
+            <Typography>Available Properties</Typography>
+          </StyledButton>
+          <StyledButton onClick={() => handleClick(2)}>
+            <Typography>FAQ</Typography>
+          </StyledButton>
         </Toolbar>
       </MUIAppBar>
     </Box>
--- src/components/MainContainer.tsx
+++ src/components/MainContainer.tsx
@@ -3,9 +3,17 @@ import * as React from "react";
 import AppBar from "./AppBar";
 
 export default function MainContainer() {
+  const [page, setPage] = React.useState<number>(0);
   return (
     <>
-      <AppBar />
+      <AppBar onPageChange={setPage} />
+      {page === 0 ? (
+        <div style={{ color: "white" }}>home</div>
+      ) : page === 1 ? (
+        <div style={{ color: "white" }}>Available Properties</div>
+      ) : (
+        <div style={{ color: "white" }}>FAQ</div>
+      )}
     </>
   );
 }
